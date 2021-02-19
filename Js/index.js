a4aeed86747f089ef4b6625cbd3fff392d0ad26a
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-02-19 12:10:26 +0100

    dropdownfunction start

---
+++ Js/index.js
@@ -0,0 +1,11 @@
+"use scrpit";
+
+//funktion för search
+
+//function för dropdown
+let btnProfil = document.querySelector(".profile");
+let dropdownProfile = document.querySelector(".dropdown-profile");
+
+btnProfil.onclick = function () {
+  dropdownProfile.style.display = "block";
+};
