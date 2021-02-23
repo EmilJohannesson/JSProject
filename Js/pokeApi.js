2dfab3f20f31fa1b680364943c0e80e849efc7bc
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-02-23 16:30:51 +0100

    name into searchbox

--- Js/pokeApi.js
+++ Js/pokeApi.js
@@ -2,45 +2,34 @@
 
 let pokemonSearch = document.querySelector(".searchList");
 
-fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
-  .then((response) => response.json())
-  .then((pokemonData) =>
-    pokemonData.results.forEach((pokemon) => fetchPokemonStats(pokemon))
-  );
-
-function fetchPokemonStats(pokemon) {
-  let url = pokemon.url;
-  fetch(url)
-    .then((response) => response.json())
-    .then((pokeData) => {
-      //console.log(pokeData);
-      const pokemonInfo = {};
-      pokemonInfo["name"] = pokeData.name;
-      console.log(pokemonInfo);
-    });
-}
-
-
-/*
-
 pokemonArray = [];
 
 fetchPokemon();
 
-function fetchPokemon () {
-fetch('https://pokeapi.co/api/v2/pokemon?limit=5')
-.then(response => response.json())
-.then(pokemonData => pokemonData.results.forEach(pokemon => {
-    let url = pokemon.url;
-    fetch(url)
-    .then(response => response.json())
-    .then(pokeData => pokemonArray.push(pokeData));
-}))
-setTimeout(function() {
+function fetchPokemon() {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+    .then((response) => response.json())
+    .then((pokemonData) =>
+      pokemonData.results.forEach((pokemon) => {
+        let url = pokemon.url;
+        fetch(url)
+          .then((response) => response.json())
+          .then((pokeData) => pokemonArray.push(pokeData));
+      })
+    );
+  setTimeout(function () {
     console.log(pokemonArray);
     console.log(pokemonArray[0]);
-}, 1000);
+
+    let text, i;
+
+    plength = pokemonArray.length;
+
+    text = "</ul>";
+    for (i = 0; i < pokemonArray.length; i++) {
+      text += "<li>" + pokemonArray[i].name + "</li>";
+    }
+    text += "</ul>";
+    pokemonSearch.innerHTML = text;
+  }, 1000);
 }
-
-
-*/
